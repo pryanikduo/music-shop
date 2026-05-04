48b18f9613f572c67e9069b68d02cff68747c37b
pryanikduo <nyan4cat@mail.ru>
2026-05-04 12:05:47 +0300

    feat: Добавлена шапка меню и подвал сайта и дизайн к ним

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,17 @@
+<header class="site-header">
+    <nav class="navigate-menu">
+        <a href="/" class="navigate-item"><span>Главная</span></a>
+        <div class="navigate-dropdown-menu">
+            <a href="/catalog" class="navigate-item"><span>Каталог</span></a>
+            <ul class="dropdown-menu">
+                <li><a href="/catalog#1">Струнные</a></li>
+                <li><a href="/catalog#2">Клавишные</a></li>
+                <li><a href="/catalog#3">Смычковые</a></li>
+                <li><a href="/catalog#4">Ударные</a></li>
+            </ul>
+        </div>
+        <a href="/stocks" class="navigate-item"><span>Акции</span></a>
+        <a href="/about-us" class="navigate-item"><span>О нас</span></a>
+        <a href="/bascet"><img src="bascet.svg"></a>
+    </nav>
+</header>
---
+++ resources/views/main.blade.php
@@ -0,0 +1,9 @@
+@include('layouts.head')
+
+@section('title', 'Главная страница')
+
+@include('layouts.menu')
+<body>
+    
+</body>
+@include('layouts.footer')
